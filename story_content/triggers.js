07cffc2cc9f64e22d81bb2d6b55b907481ecb542
Taylor45 <100591245+Taylor45@users.noreply.github.com>
2025-09-22 10:51:26 +0200

    2025/09/22

---
+++ story_content/triggers.js
@@ -0,0 +1,43 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6fa1RLNsKAH":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  const target = object('6JbX7Gx0ao8');
+const duration = 750;
+const easing = 'cubic-bezier(0.25, 1, 0.5, 1)';
+const id = '6gEThkUeEyU';
+const growAmount = 1.2;
+player.addForTriggers(
+id,
+target.animate(
+player.emphasis.elastic(growAmount)
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
